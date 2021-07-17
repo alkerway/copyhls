a1be6ecd53978b89474c298a71a05cd7c7e30c8e
alkerway <alex@walkeralex.com>
2021-07-17 12:16:34 +1200

    allow setting url and referer via command line args

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,14 +1,15 @@
-export const levelUrl = `
-https://nzp-ms04.si.edu/live_edge_panda/smil:panda03_all.smil/chunklist_w1394146429_b1264000.m3u8
+const [_1, _2, url, ref] = process.argv
+
+export const levelUrl = url || `
 `.trim()
-export const referer = `
+export const referer = ref || `
 `.trim()
-export const stopAfter = 60 * 60 * 6
+export const stopAfter = 60 * 60 * 0.01
 export const levelPollInterval = 6
 export const storageBase = 'manifest'
 
 export const outputFormat = 'mp4'
 
 export const maxConcurrentDownloads = 6
-export const maxStallCount = 10
-export const maxNetworkError = 4
+export const maxStallCount = 5
+export const maxNetworkError = 3
